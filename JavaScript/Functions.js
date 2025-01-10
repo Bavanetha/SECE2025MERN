bcc3cf0a8915cc7d3fd5e35870ad4dee93d35f16
bava <bavanethamr@gmail.com>
2025-01-10 19:27:11 +0530

    day5 task added

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -1,7 +1,26 @@
+//with parameter and with return
 function SECE(dept){
-    console.log("The dept is : ",dept);
     return "Section of the department is A";
 }
-
 console.log(SECE("cse"));
+
+//with parameter and without return
+function SECE2025(dept){
+    console.log("The dept is : ",dept);
+}
+SECE("cse");
+
+//without parameter and with return
+function Add(){
+    return "Addion";
+}
+console.log(Add());
+
+//without parameter and without return
+function sub(){
+    console.log("Subtraction");
+
+}
+sub();
+
 
